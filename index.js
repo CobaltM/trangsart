50d93d8d2ab247bf3a5b732fbc48bbb0fcb39ab1
CobaltM <ethan112096@gmail.com>
2023-05-19 10:55:57 -0500

    this is a ninor commit

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ app.get('/', function (req, res) {
   { title : 'Home' }
   )
 })
+app.get('/holden-ham-crochet', function (req, res) {
+  res.render('holden-ham-crochet.html',
+  { title : 'Home' }
+  )
+})
+app.get('/prints', function (req, res) {
+  res.render('prints.html',
+  { title : 'Home' }
+  )
+})
+app.get('/paintings', function (req, res) {
+  res.render('paintings.html',
+  { title : 'Home' }
+  )
+})
+app.get('/ceramics', function (req, res) {
+  res.render('ceramics.html',
+  { title : 'Home' }
+  )
+})
 
 app.listen(3000)
 app.listen(8080, () => {
